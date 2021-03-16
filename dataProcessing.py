812c8742c81cef4735bf2422f771593f73b71145
Dominick <dominick.rose@aggiemail.usu.edu>
2021-03-16 16:41:53 -0600

    Additional data processing and visualization

--- dataProcessing.py
+++ dataProcessing.py
@@ -6,7 +6,19 @@ import pandas as pd
 def readData():
 	dataset = pd.read_csv('data/Sample.csv')
 	dataset_array = dataset.iloc[:, 1:].to_numpy()
-	plt.plot(dataset_array[0], dataset_array[1:])
+	
+	#Adjust final column to keep units consistent 
+	dataset_array[:, -1] *= 1000
+
+	#All entries with negative numbers in second column are garbage
+	dataset_array = dataset_array[dataset_array[..., 1] >= 0]
+
+	#Calculate the total demand of the lab
+	dataset_array[:, -1] += dataset_array[:, 1]
+	print(dataset_array)
+	
+	plt.plot(dataset_array[0], dataset_array[1], color = 'red', label = 'Generated Solar Power')
+	plt.plot(dataset_array[0], dataset_array[2], color = 'blue', label = 'Lab Electrical Demand')
 	plt.show()
 
 readData()
