c72cfca3436a0a49c03ff0079ecd3e18bb55b251
Dominick <dominick.rose@aggiemail.usu.edu>
2021-03-19 00:22:34 -0600

    Formatted data into np array to use with neural network

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,24 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from datetime import datetime
 
-#Read CSV file and transfer into a numpy array with valid info
+#Read CSV file and transfer into a numpy array
 def readData():
-	dataset = pd.read_csv('data/Sample.csv')
-	dataset_array = dataset.iloc[:, 1:].to_numpy()
-	
+	dataset = pd.read_csv('data/Sample.csv',  parse_dates=[1])
+	dataset['month'] = pd.DatetimeIndex(dataset['TIME']).month
+	dataset['day'] = pd.DatetimeIndex(dataset['TIME']).day
+	dataset['hour'] = pd.DatetimeIndex(dataset['TIME']).hour
+	dataset['minute'] = pd.DatetimeIndex(dataset['TIME']).minute
+	dataset['second'] = pd.DatetimeIndex(dataset['TIME']).second
+	dataset['day_of_week'] = pd.DatetimeIndex(dataset['TIME']).dayofweek
+	dataset_array = dataset.iloc[:, 2:].to_numpy()
+	return dataset_array
+
+#Clean data by removing grabage values and normalizing units
+def cleanData(dataset_array):
 	#Adjust final column to keep units consistent 
-	dataset_array[:, -1] *= 1000
+	dataset_array[:, 1] *= 1000
 
 	#All entries with negative numbers in second column are garbage
-	dataset_array = dataset_array[dataset_array[..., 1] >= 0]
+	dataset_array = dataset_array[dataset_array[..., 0] >= 0]
 
 	#Calculate the total demand of the lab
-	dataset_array[:, -1] += dataset_array[:, 1]
-	print(dataset_array)
-	
+	dataset_array[:, 1] += dataset_array[:, 0]
+	return dataset_array[:, 1:]
+
+	'''
 	plt.plot(dataset_array[0], dataset_array[1], color = 'red', label = 'Generated Solar Power')
 	plt.plot(dataset_array[0], dataset_array[2], color = 'blue', label = 'Lab Electrical Demand')
 	plt.show()
+	'''
 
-readData()
+test = readData()
+test = cleanData(test)
+print(test[0])
